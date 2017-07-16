0cfa9a4813b8a697c7dae2381d476854d14095f0
AMITHA CHALAPPURAM <amithachalappuram@gmail.com>
2017-07-16 14:49:33 +0530

    Add files via upload

---
+++ appendinsertlist.py
@@ -0,0 +1,7 @@
+months=['jan','feb','march','april']
+months.append('may')
+months.append('jul')
+print 'the adding months list is',months
+months.insert(5,'jun')
+print 'the current list is',months
+
